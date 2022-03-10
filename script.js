2b27a2dbf5959876b6dce54c1cc984b93483b326
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-03-10 13:26:51 +0100

    Styling

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ get_meal_btn.addEventListener('click', () => {
 	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 		.then(res => res.json())
 		.then(res => {
-		createMeal(res.meals[0]);
-	});
+			createMeal(res.meals[0]);
+		});
 });
 
 const createMeal = (meal) => {
 	const ingredients = [];
 	// Get all ingredients from the object. Up to 20
-	for(let i=1; i<=20; i++) {
-		if(meal[`strIngredient${i}`]) {
+	for (let i = 1; i <= 20; i++) {
+		if (meal[`strIngredient${i}`]) {
 			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
 		} else {
 			// Stop if no more ingredients
 			break;
 		}
 	}
-	
+
 	const newInnerHTML = `
 		<div class="row">
 			<div class="columns five">
@@ -46,8 +46,7 @@ const createMeal = (meal) => {
 				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
 				</iframe>
 			</div>
-		</div>` : ''}
-	`;
-	
+		</div>` : ''}`;
+
 	meal_container.innerHTML = newInnerHTML;
 }
