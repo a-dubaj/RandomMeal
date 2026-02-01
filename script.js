8cb7fda58d406e270615df169e18e2a81aca286c
adubaj <dubaj.andrzej@gmail.com>
2026-02-01 17:53:31 +0100

    refactor: remove comments and improve code formatting
    - Remove all code comments for cleaner codebase
    - Standardize indentation across all files
    - Add semicolon to createMeal function for consistency
    - Improve overall code readability

--- script.js
+++ script.js
@@ -11,12 +11,11 @@ get_meal_btn.addEventListener('click', () => {
 
 const createMeal = (meal) => {
 	const ingredients = [];
-	// Get all ingredients from the object. Up to 20
+
 	for (let i = 1; i <= 20; i++) {
 		if (meal[`strIngredient${i}`]) {
-			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
 		} else {
-			// Stop if no more ingredients
 			break;
 		}
 	}
@@ -49,4 +48,4 @@ const createMeal = (meal) => {
 		</div>` : ''}`;
 
 	meal_container.innerHTML = newInnerHTML;
-}
+};
